4b779d787c9bef9a46b80b968740482f368e8d66
thinhcu67si <94957063+thinhcu67si@users.noreply.github.com>
2024-08-09 20:23:36 +0700

    Add User-service
    Add User-service, skaffold, front-end page, infra

---
+++ backend/user-service/src/dbConfig.ts
@@ -0,0 +1,19 @@
+export interface DBConfig {
+  host: string;
+  port: number;
+  database: string;
+  user: string;
+  password: string;
+}
+
+const config: DBConfig = {
+  host: 'postgres-user-srv',
+  port: 5432,
+  database: 'auth-db',
+  user: 'auth-admin',
+  password: 'auth-admin',
+};
+
+export default config;
+// ok roi do, chay sai  port =)):))
+
